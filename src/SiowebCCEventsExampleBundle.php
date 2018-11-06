bbc64dea1d35b520f883d9f8091c7af43aaf615d
Sascha Weidner <support@sioweb.de>
2018-11-06 15:35:02 +0100

    Create SiowebCCEventsExampleBundle.php

---
+++ src/SiowebCCEventsExampleBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sioweb\CCEventsExample;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Sascha Weidner <http://www.sioweb.de>
+ */
+class SiowebCCEventsExampleBundle extends Bundle
+{
+}
